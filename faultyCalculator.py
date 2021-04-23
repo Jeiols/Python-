7dd010408bb0fa1278d66793059fe71fa245187d
Jeiols <70755103+Jeiols@users.noreply.github.com>
2021-04-23 21:18:30 +0100

    Add files via upload
    code for a faulty calculator, lowkey long not enough simplified but ill get there i just started.

---
+++ faultyCalculator.py
@@ -0,0 +1,22 @@
+# 4*3 = 555, 56+9 = 77, 56/6 = 4
+a=(input("number 1: "))
+a1=(input("number 2: "))
+a =int(a)
+b =int(a1)
+import operator
+oper = input("choose the operator : (+,-,*,/)")
+if a ==54 and b ==4 and oper =="+":
+    print("17")
+elif oper=="+":
+     print(a + b)
+elif oper=="-":
+    print(a-b)
+elif a==4 and b==9 and oper =="*":
+    print("482")
+elif oper=="*":
+    print(a*b)
+elif oper=="/" and a == 7 and b == 5:
+    print("8")
+elif oper=="/":
+    print(a/b)
+# This is crap mate istg
